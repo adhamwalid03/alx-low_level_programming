d677efe5af7ddfc1a4078feb2871596f49b6a157
Adhamwalid03 <vivaegypte3333@gmail.com>
2023-02-16 12:27:25 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - printing syntence
+ * Description: using main to print syntence
+ * This program just print the follwoing syetence
+ * Programming is like building a multilingual puzzle
+ * Return: 0
+ */
+
+int main(void)
+{
+	char str_var[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(str_var);
+	return (0);
+}
